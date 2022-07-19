24d471c129893d5c35b94f1958f5cdd730d0f03b
Victor Avila <victoravila1997@gmail.com>
2022-07-19 11:39:41 -0500

    Added shop link to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,16 @@
+import { Link } from "react-router-dom";
+
+import "../styles/Home.css";
+
 const Home = () => {
   return (
-    <div className="home">Home Page</div>
+    <div className="home">
+      <Link className="shop-link" to="/shop">
+        <div>
+          Shop Now
+        </div>
+      </Link>
+    </div>
   )
 }
 
